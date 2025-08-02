6156c8e296aa72cb3503524b700209934f40e462
xbb4u <118782227+xbb4u@users.noreply.github.com>
2025-08-02 23:00:01 +0100

    Updated qbx_core sql

--- qbx_core.sql
+++ qbx_core.sql
@@ -30,14 +30,23 @@ CREATE TABLE IF NOT EXISTS `bans` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `license` varchar(50) DEFAULT NULL,
+  `license2` varchar(50) DEFAULT NULL,
   `discord` varchar(50) DEFAULT NULL,
+  `xbl` varchar(50) DEFAULT NULL,
+  `live` varchar(50) DEFAULT NULL,
+  `fivem` varchar(50) DEFAULT NULL,
   `ip` varchar(50) DEFAULT NULL,
+  `tokens` text DEFAULT NULL,
   `reason` text DEFAULT NULL,
   `expire` int(11) DEFAULT NULL,
   `bannedby` varchar(255) NOT NULL DEFAULT 'LeBanhammer',
   PRIMARY KEY (`id`),
   KEY `license` (`license`),
+  KEY `license2` (`license2`),
   KEY `discord` (`discord`),
+  KEY `xbl` (`xbl`),
+  KEY `live` (`live`),
+  KEY `fivem` (`fivem`),
   KEY `ip` (`ip`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
